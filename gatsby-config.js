1039cb3c64595bf4b3bf243db672a8623e4500ca
Rishab Sharma <rishabsharma@fynd.com>
2020-07-21 08:03:46 +0530

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const config = require('./config/website');
 
 module.exports = {
+  pathPrefix: '/',
   siteMetadata: {
     title: config.siteTitle,
     description: config.siteDescription,
